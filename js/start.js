647a3eb0b33a52571f6fff30a02381ee57992a7b
Felix Nithammer <felixnithammer@Felixs-MacBook-Pro.local>
2018-09-28 15:58:09 +0200

    Major design changes

---
+++ js/start.js
@@ -0,0 +1,57 @@
+$.StartScreen = function(){
+    var plugin = this;
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+    plugin.init = function(){
+        setTilesAreaSize();
+        if (width > Metro.media_sizes.LD) addMouseWheel();
+    };
+
+    var setTilesAreaSize = function(){
+        var groups = $(".box-group");
+        var tileAreaWidth = 80;
+        $.each(groups, function(i, t){
+            if (width <= Metro.media_sizes.LD) {
+                tileAreaWidth = width;
+            } else {
+                tileAreaWidth += $(t).outerWidth() + 80;
+            }
+        });
+        $(".box-area").css({
+            width: tileAreaWidth
+        });
+    };
+
+    var addMouseWheel = function (){
+        $("body").mousewheel(function(event, delta, deltaX, deltaY){
+            var page = $(".start-screen");
+            var scroll_value = delta * 50;
+            page.scrollLeft(page.scrollLeft() - scroll_value);
+            return false;
+        });
+    };
+
+    plugin.init();
+};
+
+$.StartScreen();
+
+$.each($('[class*=box-]'), function(){
+    var tile = $(this);
+    setTimeout(function(){
+        tile.css({
+            opacity: 1,
+            "transform": "scale(1)",
+            "transition": ".3s"
+        }).css("transform", false);
+
+    }, Math.floor(Math.random()*500));
+});
+
+$(".box-group").animate({
+    left: 0
+});
+
+$(window).on(Metro.events.resize + "-start-screen-resize", function(){
+    $.StartScreen();
+});
